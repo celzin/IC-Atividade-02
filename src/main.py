a5dfd2e9f115878a10319df6f4396fe3291ea06a
Celso WSL <celsovinicius4@gmail.com>
2024-11-15 15:04:24 -0300

    fix[main]: alterando estruturas para correto funcionamento

--- src/main.py
+++ src/main.py
@@ -1,24 +1,23 @@
-import numpy as np
 from data_generator import generate_data
 from fuzzy_system import FuzzySystemTS
 from evaluation import calculate_mse, calculate_rmse, plot_comparison, plot_error
 
-# Etapa 1: Gerar os Dados com 1000 pontos
-x, y_true = generate_data(num_points=1000)
+# Passo 1: Gerar os dados
+x, y_true = generate_data()
 
-# Etapa 2: Inicializar o Sistema Fuzzy
+# Passo 2: Inicializar o sistema fuzzy
 fuzzy_system = FuzzySystemTS(num_rules=5, order=0)
-fuzzy_system.define_membership_functions((x.min(), x.max()))
+fuzzy_system.define_membership_functions((min(x), max(x)))
 
-# Etapa 3: Aproximar a Função
-y_pred = np.array([fuzzy_system.infer(xi) for xi in x])
+# Passo 3: Aproximar a função
+y_pred = [fuzzy_system.infer(xi) for xi in x]
 
-# Etapa 4: Avaliar o Modelo
+# Passo 4: Avaliar o modelo
 mse = calculate_mse(y_true, y_pred)
 rmse = calculate_rmse(y_true, y_pred)
-print(f'MSE: {mse}')
-print(f'RMSE: {rmse}')
+print(f"MSE: {mse}")
+print(f"RMSE: {rmse}")
 
-# Etapa 5: Plotar Resultados
+# Passo 5: Visualizar os resultados
 plot_comparison(x, y_true, y_pred)
 plot_error(x, y_true, y_pred)
