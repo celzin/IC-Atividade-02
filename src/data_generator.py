217bacc0cafb536bbfb01d55cce5cfbbeaee7f6d
Celso WSL <celsovinicius4@gmail.com>
2024-11-15 15:32:39 -0300

    refactor[style]: refazendo comentários

--- src/data_generator.py
+++ src/data_generator.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def generate_data(start=0, end=10, num_points=1000):
     """
-    Generate x values and corresponding f(x) values.
+    Gera valores de x e os respectivos valores de f(x) no intervalo de [0,10] e 1000 pontos.
     """
     x = np.linspace(start, end, num_points)
     y = np.exp(-x / 5) * np.sin(3 * x) + 0.5 * np.sin(x)
